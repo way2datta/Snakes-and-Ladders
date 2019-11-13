c64192717688400f7dcb916f160d9367800045b6
Dattatray Kale <way2datta@gmail.com>
2019-11-13 20:29:51 +0530

    .net core version has been added

---
+++ csharp/.net-core/SnakesAndLadders.ClassLibrary/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakesAndLadders.ClassLibrary
+{
+    public class Calculator
+    {
+        public static int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ csharp/.net-core/SnakesAndLadders.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SnakesAndLadders.ClassLibrary;
+
+namespace SnakesAndLadders.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(4, 5));
+        }
+    }
+}
